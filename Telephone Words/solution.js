a195ff383185cef9f307a3e65a6d2f6dd71b73e4
Kevin Nguyen <kevinn858@gmail.com>
2019-11-21 10:48:31 -0800

    updated Telephone Words solution with another test

--- Telephone Words/solution.js
+++ Telephone Words/solution.js
@@ -11,24 +11,26 @@ digits_to_letters = {
     9: 'WXYZ'
   }
   
-  function telephone_words(digits) {
-    let answer = [];
-    const firstSet = digits_to_letters[digits.charAt(0)];
-  
-    for (let i = 0; i < firstSet.length; i++) {
-      answer.push(firstSet.charAt(i));
-    }
-    for (let i = 1; i < digits.length; i++) {
-      const newAnswer = [];
-      answer.forEach(element => {
-        let charSet = digits_to_letters[digits[i]];
-        for (let j = 0; j < charSet.length; j++) {
-          newAnswer.push(element + charSet[j]);
-        }
-      })
-      answer = newAnswer;
-    }
-    return answer;
+function telephone_words(digits) {
+  let answer = [];
+  const firstSet = digits_to_letters[digits.charAt(0)];
+
+  for (let i = 0; i < firstSet.length; i++) {
+    answer.push(firstSet.charAt(i));
   }
-  
-  console.log(telephone_words('2735'));
+  for (let i = 1; i < digits.length; i++) {
+    const newAnswer = [];
+    answer.forEach(element => {
+      let charSet = digits_to_letters[digits[i]];
+      for (let j = 0; j < charSet.length; j++) {
+        newAnswer.push(element + charSet[j]);
+      }
+    })
+    answer = newAnswer;
+  }
+  return answer;
+}
+
+// tests
+console.log(telephone_words('27'));
+console.log(telephone_words('2735'));
